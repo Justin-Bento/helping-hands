51c537d37a5ea1f6f1e0f683283f24ee9dc762cc
Justin Bento <bento.a.justin@gmail.com>
2023-03-23 13:45:09 -0400

    replaced tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <main className="">
+        <section className="">
+          <h1 className="text-3xl font-bold underline">Section 01!</h1>
+        </section>
+        {/* Section 01:  */}
+        <section className="">
+          <h2 className="text-3xl font-bold underline">Section 02!</h2>
+        </section>
+        {/* Section 01:  */}
+        <section className="">
+          <h3 className="text-3xl font-bold underline">Section 03!</h3>
+        </section>
+        {/* Section 01:  */}
+        <section className="">
+          <h4 className="text-3xl font-bold underline">Section 03!</h4>
+        </section>
+        {/* Section 01:  */}
+        <section className="">
+          <h5 className="text-3xl font-bold underline">Section 04!</h5>
+        </section>
+        {/* Section 01:  */}
+        <section className="">
+          <h6 className="text-3xl font-bold underline">Section 05!</h6>
+        </section>
+        {/* Section 01:  */}
+      </main>
+    </>
   )
 }
