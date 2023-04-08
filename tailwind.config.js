85766b854860a1755daa47872970c4b7f32ebf5d
Justin Bento <bento.a.justin@gmail.com>
2023-04-08 17:38:48 -0400

    included brand colors and images

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,28 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#f4f7fb",
+          100: "#e8eef6",
+          200: "#cbddec",
+          300: "#9ec0db",
+          400: "#6aa0c6",
+          500: "#4784b0",
+          600: "#356a94",
+          700: "#2c5578",
+          800: "#284864",
+          900: "#263e54",
+          950: "#192938",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
